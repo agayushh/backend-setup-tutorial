13bb77dfbb5b8fd6718632f4af61eb4fd2d7209c
agayushh <agayush088@gmail.com>
2025-07-20 19:42:06 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -21,6 +21,15 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
 app.use(express.static("public"));
 
-app.use(cookieParser())
+app.use(cookieParser());
+
+//routes import
+
+import userRouter from "./routes/user.route.js";
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
+
+//http:localhost:8000/api/v1/users/register
 
 export default app;
